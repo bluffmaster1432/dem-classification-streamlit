3d708b996b77696524adf25cf5ce1b5600991ee3
Bluff Master <40918868+bluffmaster1432@users.noreply.github.com>
2022-05-16 20:40:51 +0530

    added jpeg,png,bmp support

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.title("Simple Image Classification Application")
 st.write("")
 
 # enable users to upload images for the model to make predictions
-file_up = st.file_uploader("Upload an image", type = "jpg")
+file_up = st.file_uploader("Upload an image", type = ["jpg","jpeg","png","bmp"])
 
 
 def predict(image):
